68704083d44cdbe880d11787d13e2f84498166bf
Robani <muhammadrobbani77@gmail.com>
2023-11-23 19:45:59 +0700

    Validasi JS

--- js/script.js
+++ js/script.js
@@ -22,21 +22,71 @@ $(function () {
     });
 
     const slides = document.querySelectorAll(".sliders .img");
-    // slides.forEach((slide, indx) => {
-    //     slide.style.transform = `translateX(${indx * 100}%)`;
-    // });
 
     let i = 0;
     setInterval(() => {
         Array.from(slides).forEach((item, index) => {
-            // Calculate the translation based on the current index
             const translation = index === i ? 0 : 100 * (index - i);
 
-            // Apply the translation to the current slide
             item.style.transform = `translateX(${translation}%)`;
         });
 
-        // Increment the index for the next iteration
         i = (i + 1) % slides.length;
-    }, 2000);
+    }, 3000);
+
+    function isFormValid() {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var option = $("#option").val();
+
+        // Validasi Nama
+        if (name === "") {
+            $("#nameError").text("Nama tidak boleh kosong");
+            $("#name").addClass('error');
+            return false;
+        } else {
+            $("#nameError").text("");
+            $("#name").removeClass('error');
+        }
+
+        // Validasi Email
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (email === "") {
+            $("#emailError").text("Email tidak boleh kosong");
+            $("#email").addClass('error');
+            return false;
+        } else {
+            if (!emailRegex.test(email)) {
+                $("#emailError").text("Format email tidak valid");
+                $("#email").addClass('error');
+                return false
+            } else {
+                $("#emailError").text("");
+                $("#email").removeClass('error');
+            }
+        }
+
+        // Validasi Pilihan
+        if (option === "Select Option") {
+            $("#optionError").text("Pilih opsi yang valid");
+            $("#option").addClass('error');
+            return false;
+        } else {
+            $("#optionError").text("");
+            $("#option").removeClass('error');
+        }
+
+        return true; // Semua validasi berhasil
+    }
+
+    // Event handler untuk memvalidasi seiring dengan pengisian input
+    $("#name, #email, #option").on("input", function () {
+        updateSubmitButton();
+    });
+    // Fungsi untuk mengaktifkan/menonaktifkan tombol berdasarkan validasi
+    function updateSubmitButton() {
+        var isFormValidResult = isFormValid();
+        $("#submitButton").prop("disabled", !isFormValidResult);
+    }
+
 });
